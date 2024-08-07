ab56c021c9b080d719d7bf710719e6bef028aeac
chipchippp <114909102+chipchippp@users.noreply.github.com>
2024-08-07 19:39:23 +0700

    update

---
+++ src/main/java/com/example/shopapp_be/ShopappBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.shopapp_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopappBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopappBeApplication.class, args);
+	}
+
+}
